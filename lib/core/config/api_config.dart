d99ce6b6daea227e98722d5c32f63413a740eeb7
46139 <datnp5@fpt.com>
2026-06-13 18:59:45 +0700

    add: API config pointing to production BE
    - baseUrl: https://vanvy.up.railway.app
    - Ready for HttpClient integration

---
+++ lib/core/config/api_config.dart
@@ -0,0 +1,6 @@
+class ApiConfig {
+  static const String baseUrl = 'https://vanvy.up.railway.app';
+  static const String apiVersion = 'api';
+  
+  static String get apiBaseUrl => '$baseUrl/$apiVersion';
+}
